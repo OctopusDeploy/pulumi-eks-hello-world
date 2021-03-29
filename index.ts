a86b5806d2ee0af5e87e0869bc5a718c1fc7c3e0
Michael Richardson <michael.richardson@hey.com>
2021-03-29 11:17:16 +1000

    updated dockerfile to include kubectl, and added guestbook deployment

--- index.ts
+++ index.ts
@@ -22,6 +22,7 @@ vpcId: vpc.id,
 
 // If we're running inside Octopus, emit a service message to output the cluster URL 
 if (config.requireBoolean("runningViaOctopus")) {
+
     console.log(
         pulumi.interpolate `set_octopusvariable "k8sClusterUrl" "${cluster.core.endpoint}"`
     )
@@ -29,3 +30,123 @@ if (config.requireBoolean("runningViaOctopus")) {
 
 // Export the clusters' kubeconfig.
 export const kubeconfig = cluster.kubeconfig
+
+// Deploy the guestbook application to the k8s cluster
+
+//
+// REDIS LEADER.
+//
+
+const redisLeaderLabels = { app: "redis-leader" };
+const redisLeaderDeployment = new k8s.apps.v1.Deployment("redis-leader", {
+    spec: {
+        selector: { matchLabels: redisLeaderLabels },
+        template: {
+            metadata: { labels: redisLeaderLabels },
+            spec: {
+                containers: [
+                    {
+                        name: "redis-leader",
+                        image: "redis",
+                        resources: { requests: { cpu: "100m", memory: "100Mi" } },
+                        ports: [{ containerPort: 6379 }],
+                    },
+                ],
+            },
+        },
+    },
+});
+const redisLeaderService = new k8s.core.v1.Service("redis-leader", {
+    metadata: {
+        name: "redis-leader",
+        labels: redisLeaderDeployment.metadata.labels,
+    },
+    spec: {
+        ports: [{ port: 6379, targetPort: 6379 }],
+        selector: redisLeaderDeployment.spec.template.metadata.labels,
+    },
+});
+
+//
+// REDIS REPLICA.
+//
+
+const redisReplicaLabels = { app: "redis-replica" };
+const redisReplicaDeployment = new k8s.apps.v1.Deployment("redis-replica", {
+    spec: {
+        selector: { matchLabels: redisReplicaLabels },
+        template: {
+            metadata: { labels: redisReplicaLabels },
+            spec: {
+                containers: [
+                    {
+                        name: "replica",
+                        image: "pulumi/guestbook-redis-replica",
+                        resources: { requests: { cpu: "100m", memory: "100Mi" } },
+                        // If your cluster config does not include a dns service, then to instead access an environment
+                        // variable to find the leader's host, change `value: "dns"` to read `value: "env"`.
+                        env: [{ name: "GET_HOSTS_FROM", value: "dns" }],
+                        ports: [{ containerPort: 6379 }],
+                    },
+                ],
+            },
+        },
+    },
+});
+const redisReplicaService = new k8s.core.v1.Service("redis-replica", {
+    metadata: {
+        name: "redis-replica",
+        labels: redisReplicaDeployment.metadata.labels
+    },
+    spec: {
+        ports: [{ port: 6379, targetPort: 6379 }],
+        selector: redisReplicaDeployment.spec.template.metadata.labels,
+    },
+});
+
+//
+// FRONTEND
+//
+
+const frontendLabels = { app: "frontend" };
+const frontendDeployment = new k8s.apps.v1.Deployment("frontend", {
+    spec: {
+        selector: { matchLabels: frontendLabels },
+        replicas: 3,
+        template: {
+            metadata: { labels: frontendLabels },
+            spec: {
+                containers: [
+                    {
+                        name: "frontend",
+                        image: "pulumi/guestbook-php-redis",
+                        resources: { requests: { cpu: "100m", memory: "100Mi" } },
+                        // If your cluster config does not include a dns service, then to instead access an environment
+                        // variable to find the master service's host, change `value: "dns"` to read `value: "env"`.
+                        env: [{ name: "GET_HOSTS_FROM", value: "dns" /* value: "env"*/ }],
+                        ports: [{ containerPort: 80 }],
+                    },
+                ],
+            },
+        },
+    },
+});
+const frontendService = new k8s.core.v1.Service("frontend", {
+    metadata: {
+        labels: frontendDeployment.metadata.labels,
+        name: "frontend",
+    },
+    spec: {
+        type: isMinikube ? "ClusterIP" : "LoadBalancer",
+        ports: [{ port: 80 }],
+        selector: frontendDeployment.spec.template.metadata.labels,
+    },
+});
+
+// Export the frontend IP.
+export let frontendIp: pulumi.Output<string>;
+if (isMinikube) {
+    frontendIp = frontendService.spec.clusterIP;
+} else {
+    frontendIp = frontendService.status.loadBalancer.ingress[0].ip;
+}
